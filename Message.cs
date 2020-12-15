a77cf003c4257d7a72c229a44bd9b32a001da883
ItzBlacky <vnwlif0@gmail.com>
2020-12-15 13:24:49 +0700

    Initial Commit

---
+++ Message.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json;
+
+namespace ItzChatClient
+{
+        public class Message
+        {
+            public string Type { get; private set; }
+            public string[] Data { get; private set; }
+            public string Sender { get; private set; }
+
+            public Message(string Type, string[] Data, string Sender = "")
+            {
+                this.Type = Type;
+                this.Data = Data;
+                this.Sender = Sender;
+            }
+
+            public string toJson()
+            {
+                return JsonSerializer.Serialize(this);
+            }
+            public static Message fromJson(string Json)
+            {
+                return JsonSerializer.Deserialize<Message>(Json);
+            }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using WebSocketSharp;
+
+namespace ItzChatClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(var ws = new WebSocket("ws://localhost:811/chat"))
+            {
+                ws.OnOpen += (sender, e) =>
+                {
+                    Console.WriteLine("Socket Opened");
+                };
+                ws.OnMessage += (sender, e) =>
+                {
+                    Console.WriteLine(e.Data);
+                };
+                ws.Connect();
+                Message message = new Message("REGISTER", new string[]{ "string1", "string2", "string3" } );
+                ws.Send(message.toJson());
+                Console.ReadKey();
+            }
+        }
+    }
+}
